2cbd9b190048bdaee4a7a6180f7dabf48ea6d7fd
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2022-08-05 12:37:52 +0200

    Rename Articles to DoubleArticles

---
+++ src/components/DoubleArticle.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="articles-container">
+        <Article />
+    </div>
+</template>
+
+<script>
+import Article from './Article.vue'
+
+</script>
+
+<style>
+.articles-container {
+    height: 100%;
+    width: 100%;
+    background-color: grey;
+    display: flex;
+    gap: 10px;
+    justify-content: space-around;
+}
+
+.article-card {
+    height: 60%;
+    width: 48%;
+    background-color: rgb(95, 95, 95);
+}
+</style>
