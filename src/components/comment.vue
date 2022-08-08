d69cf598fe9160e52336a96a18ca123894cc89d1
Edgar M <evmelendrez@gmail.com>
2022-08-08 15:39:29 -0700

    post comment when submit is hit (local only)

---
+++ src/components/comment.vue
@@ -0,0 +1,35 @@
+<script>
+// local functions can be decalred here
+export default {
+    props: {
+        // properties go here
+            // ex: title: String,
+            timestamp: Date,
+            poster: Object,
+            replies: Array,
+            comment: String,
+    },
+    data() {
+        return {
+            // responsive variables go here
+                // ex: count: 0,
+        }
+    },
+    methods : {
+        // callable functions for HTML go here
+            // ex: incCount()
+            // {
+            //     this.count++;
+            // }
+    }
+}
+</script>
+<template>
+<!-- HTML for components goes here -->
+<h4 class="commentOwner">{{poster.userName}}</h4>
+<p>{{timestamp}}--{{comment}}</p>
+<p>{{}}</p>
+</template>
+<style scoped>
+    /* Styles for component go here */
+</style>
--- src/components/commentBox.vue
+++ src/components/commentBox.vue
@@ -1,44 +1,60 @@
 <script>
+import Comment from './comment.vue'
 // local functions can be decalred here
+
 export default {
     props: {
-        // properties go here
-            // ex: title: String,
-            postedComments: 
-            [
-                {timeStamp: NaN, poster: NaN, responseToo: NaN, comment: NaN},
-                {},
-            ],
+    // properties go here
+    // ex: title: String,
     },
     data() {
         return {
             // responsive variables go here
-                // ex: count: 0,
+            // ex: count: 0,
             desiredComment: undefined,
+            postedComments: [
+                { timeStamp: NaN, poster: NaN, replies: [], comment: "te8st" },
+                { timeStamp: NaN, poster: NaN, replies: [], comment: "test" },
+            ],
+        };
+    },
+    methods: {
+        // callable functions for HTML go here
+        // ex: incCount()
+        // {
+        //     this.count++;
+        // }
+        submitComment() {
+            const time = this.getTime();
+            this.postedComments.push({ timestamp: new Date(), poster: NaN, responseToo: NaN, comment: this.desiredComment },);
+            alert(time);
+        },
+        getTime() 
+        {
+            const calendar = new Date();
+            // alert(calendar);
+            return calendar;
         }
     },
-    methods : {
-        // callable functions for HTML go here
-            // ex: incCount()
-            // {
-            //     this.count++;
-            // }
-        submitComment()
-        {
-            // alert('sdfsd')
-                  
-        }
-    }
+    components: { Comment }
 }
 </script>
 <template>
 <!-- HTML for components goes here -->
     <div id="commentWrapper">
         <div id="displayCommentBox">
-            <h1>Posted comments would go here</h1>
+            <div v-for="(comment, index) in postedComments">
+                <Comment 
+                    :timeStamp="postedComments[index].timeStamp"
+                    :poster = "{}"
+                    :replies="[]"
+                    :comment="postedComments[index].comment"
+                ></Comment>
+            </div>
         </div>       
         <div id="commentMaker">
-            <p>Where the comment would be typed initialy: {{desiredComment}}</p>
+            <p>Comment: </p>
+            <!-- <p>Where the comment would be typed initialy: {{desiredComment}}</p> -->
             <textarea v-model="desiredComment" placeholder="Comment..."></textarea>
             <button class="commentSubmit" @click="submitComment">&#x27A1</button>
         </div>
