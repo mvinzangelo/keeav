344aebb7d00a75c7c2ea193e5d145f59a5804582
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2022-08-09 20:02:46 +0200

    Move link to data field

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -24,24 +24,25 @@ export default {
             articlePictureLink: "src/assets/placeholderImage.svg",
             author: "[Author]",
             date: "mm/dd/yyyy",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce porta felis ut elementum vehicula. Praesent malesuada finibus nunc, vel mollis risus vestibulum et. Pellentesque consectetur tincidunt gravida. Suspendisse gravida justo at arcu vehicula dignissim. Cras tempor orci tempor mollis vulputate. Sed varius lectus sed interdum tincidunt. In nec eros fringilla purus tristique finibus sit amet ut nisl. Nullam semper rutrum nunc, ac blandit."
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce porta felis ut elementum vehicula. Praesent malesuada finibus nunc, vel mollis risus vestibulum et. Pellentesque consectetur tincidunt gravida. Suspendisse gravida justo at arcu vehicula dignissim. Cras tempor orci tempor mollis vulputate. Sed varius lectus sed interdum tincidunt. In nec eros fringilla purus tristique finibus sit amet ut nisl. Nullam semper rutrum nunc, ac blandit.",
+            link: "https://edition.cnn.com/2022/08/06/europe/zaporizhzhia-nuclear-plant-intl/index.html"
+            // link: {
+            //     type: String,
+            //     default: "https://edition.cnn.com/2022/08/06/europe/zaporizhzhia-nuclear-plant-intl/index.html",
+            //     // validation function by https://stackoverflow.com/users/1092711/pavlo on https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url
+            //     validator(value) {
+            //         let url;
+            //         try {
+            //             url = new URL(value);
+            //         } catch (_) {
+            //             return false;
+            //         }
+            //         return url.protocol === "http:" || url.protocol === "https:";
+            //     }
+            // }
         }
     },
     props: {
-        link: {
-            type: String,
-            default: "https://edition.cnn.com/2022/08/06/europe/zaporizhzhia-nuclear-plant-intl/index.html",
-            // validation function by https://stackoverflow.com/users/1092711/pavlo on https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url
-            validator(value) {
-                let url;
-                try {
-                    url = new URL(value);
-                } catch (_) {
-                    return false;
-                }
-                return url.protocol === "http:" || url.protocol === "https:";
-            }
-        }
     }
 }
 
