bbac5a4f57989f3059c967652193e76d6a44bd75
cooled909 <esegu007@ucr.edu>
2022-08-10 08:36:14 +0200

    created basic front-end voting

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import Voting from './components/Voting.vue'
 </script>
 
 <template>
@@ -9,14 +10,13 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
+      <Voting newsOrg="Fox News" author="John Doe" userVote="None"/>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
   </header>
-
   <RouterView />
 </template>
 
---
+++ src/components/Voting.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+    data() {
+        return {
+            userVote: 'None'
+        }
+    },
+    props: {
+        newsOrg: String,
+        author: String,
+        userVote: String
+    }
+}
+</script>
+
+<template>
+<div class="container">
+    <h1>Vote: {{ userVote }}</h1>
+    <div class="buttonContainer" v-if="userVote == 'None'">
+        <button class="right" @click="userVote = 'Left'">Left</button>
+        <button class="left" @click="userVote = 'Right'">Right</button>
+    </div>
+    <div class="resultContainer" v-else>
+        <h2 class="title">Bias Meter</h2>
+        <h3 class="news">News Organization: {{newsOrg}}</h3>
+        <meter id="newsBias" min="0" max="100" value="50"></meter>
+        <h3 class>Author: {{author}}</h3>
+        <meter id="newsBias" min="0" max="100" value="50"></meter>
+        <h3>Users</h3>
+        <meter id="newsBias" min="0" max="100" value="50"></meter>
+    </div>
+</div>
+</template>
+
