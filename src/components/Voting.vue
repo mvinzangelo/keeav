e5f817edd0dae1a877d04ce7f99f809740a13764
cooled909 <esegu007@ucr.edu>
2022-08-11 22:16:59 +0200

    implemented firestore calls for Voting.vue

--- src/components/Voting.vue
+++ src/components/Voting.vue
@@ -1,16 +1,43 @@
 <script>
+import {db} from '../firebaseResources';
+import {doc, getDoc} from 'firebase/firestore';
 export default {
     data() {
         return {
+            author: '',
+            authorBias: '',
+            publisher: '',
+            publisherBias: '',
+            totalVotesLeft: '',
+            totalVotesRight: '',
             userVote: 'None'
         }
     },
     props: {
-        newsOrg: String,
-        author: String,
-        userVote: String
+        articleID: String
+    },
+    created() {
+        this.getData()
+    },
+    methods: {
+        async getData() {
+
+            const docSnap = await getDoc(doc(db,'articles',this.articleID));
+
+            if(docSnap.exists()){
+                this.author = docSnap.data().author;
+                this.authorBias = docSnap.data().authorBias;
+                this.publisher = docSnap.data().publisher;
+                this.publisherBias = docSnap.data().publisherBias;
+                this.totalVotesLeft = docSnap.data().totalVotesLeft
+                this.totalVotesRight = docSnap.data().totalVotesRight;
+            }
+            else {
+                console.log('doc does not exist');
+            }
+        }
     }
-}
+};
 </script>
 
 <template>
@@ -22,7 +49,7 @@ export default {
     </div>
     <div class="resultContainer" v-else>
         <h2 class="title">Bias Meter</h2>
-        <h3 class="news">News Organization: {{newsOrg}}</h3>
+        <h3 class="news">News Organization: {{publisher}}</h3>
         <meter id="newsBias" min="0" max="100" value="50"></meter>
         <h3 class>Author: {{author}}</h3>
         <meter id="newsBias" min="0" max="100" value="50"></meter>
