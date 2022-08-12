73d50ff78a9ca5022a39bf3a892b68036455c1e5
Alex Hsieh <alexchsieh@yahoo.com>
2022-08-12 22:12:56 +0200

    formatted file names, functional logo, and profile page

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,6 +1,11 @@
 <script>
+import { mapStores } from 'pinia';
+import { useLoginStore } from "../stores/loginStatus";
 import { RouterLink, RouterView } from 'vue-router'
 export default {
+  computed: {
+    ...mapStores(useLoginStore),
+  },
   methods: {
     currentDateTime() {
       const current = new Date();
@@ -19,13 +24,18 @@ export default {
 <template>
   <main>
     <div class="headerbox">
-      <img alt="Vue logo" class="logo" src="@/assets/hammer.svg" width="90" />
+      <button>
+        <RouterLink to="/"><img alt="Vue logo" class="logo" src="@/assets/hammer.svg" width="90" /></RouterLink>
+      </button>
       <div class="namebox">
         <h1>Hammer</h1>
         <p>{{ currentDateTime() }}</p>
       </div>
-      <button>
+      <button v-if="this.loginStore.userID == ''">
         <RouterLink to="/login"><img alt="Vue logo" class="logo" src="@/assets/human.svg" width="70" /></RouterLink>
+      </button>
+      <button v-else>
+        <RouterLink to="/profile"><img alt="Vue logo" class="logo" src="@/assets/human.svg" width="70" /></RouterLink>
       </button>
       <!-- <div class="profilepic"> -->
     </div>
