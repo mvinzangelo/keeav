b6716885be9b81b9155709b968469f38566e99a9
Edgar M <evmelendrez@gmail.com>
2022-08-15 15:13:48 -0700

    css revamp for mulitple components

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,10 +1,13 @@
 <template>
     <p class="topic-text"><span>{{ topic }}</span></p>
-    <div class="articles-container">
-        <Article :articleRef="leftArticle" ref="leftArticle"></Article>
-        <Article :articleRef="rightArticle" ref="rightArticle"></Article>
+    <div id="section-container">
+        <div class="articles-container">
+            <Article :articleRef="leftArticle" ref="leftArticle"></Article>
+            <Article :articleRef="rightArticle" ref="rightArticle"></Article>
+        </div>
+        <CommentGenerate :topicId="topicID"></CommentGenerate>
+
     </div>
-    <CommentGenerate :topicId="topicID"></CommentGenerate>
 </template>
 
 <!-- Is there a better way of doing this? -->
@@ -70,17 +73,50 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Playfair+Display:wght@500&display=swap');
 .articles-container {
-    background-color: grey;
+    margin: 0 auto;
+    width: 100%;
+    /* background-color: rgb(105, 34, 34); */
+    
     display: flex;
-    justify-content: space-around;
+    /* padding: 10px */
+    justify-content: center;
+}
+#section-container
+{
+    width: calc(100% - 500px );
+    /* background-color: darksalmon; */
+    /* display: flex; */
+    /* justify-content: center; */
+    margin: 0 auto;
 }
 
 .article-card {
     background-color: rgb(95, 95, 95);
+
 }
 
 .topic-text {
     text-align: center;
+    font-size: 40px;
+    font-weight: bold;
+    font-family: 'Playfair Display', serif;
+    /* background-color: red; */
+    text-decoration: 3px underline goldenrod;
+    padding: 20px
+}
+
+@media screen and (max-width: 1125px) {
+  .articles-container {
+    width: 100%;
+    /* background-color: saddlebrown; */
+    display: inline;
+  }
+  #section-container
+    {
+        width: calc(80% - 100px);
+    }
+ 
 }
 </style>
