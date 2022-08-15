f2589d3f6ff7e3143806e324eee31614a73d5167
Alex Hsieh <alexchsieh@yahoo.com>
2022-08-15 22:32:47 +0200

    fixed syntax and removed lines

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,6 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import { mapStores } from 'pinia'
-import { useLoginStore } from "@/stores/loginStatus";
+import { RouterView } from 'vue-router'
 import Header from '../src/components/Header.vue'
-</script>
-
-<script>
-export default {
-  computed: {
-    ...mapStores(useLoginStore),
-  },
-}
-import Voting from './components/Voting.vue'
 </script>
 
 <template>
@@ -28,11 +17,6 @@ import Voting from './components/Voting.vue'
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 10px;
-}
-
-.links {
-  display: flex;
   gap: 10px;
 }
 </style>
