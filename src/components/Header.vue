8d14392079766f6db6df1fcee84b9f41a1944b86
Kirsten821 <kirstenswilliams821@gmail.com>
2022-08-17 16:42:23 +0200

    Fixed timestamp issue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,12 +9,12 @@ export default {
     ...mapStores(useLoginStore),
   },
   methods: {
-    currentDateTime() {
+    /*currentDateTime() {
       const current = new Date();
       const date = current.getFullYear() + '-' + (current.getMonth() + 1) + '-' + current.getDate();
       const dateTime = date;
-      return dateTime;
-    },
+      return dateTime; 
+    },*/
     signOut() {
       signOut(auth).then(() => {
         this.loginStore.$reset();
