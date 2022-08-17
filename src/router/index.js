172c44379c56d2dad80ac339d36ae04372856f77
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2022-08-17 16:20:21 +0200

    Add param to home route

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/:topicID',
       name: 'home',
       component: () => import('@/views/HomeView.vue')
     },
