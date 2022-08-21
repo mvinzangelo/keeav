9cde1697c8052364be7e1d1fe36c27f9460caa25
cooled909 <esegu007@ucr.edu>
2022-08-21 16:28:09 +0200

    styled buttons and removed author bias meter

--- src/components/Voting.vue
+++ src/components/Voting.vue
@@ -86,42 +86,75 @@ export default {
 
 <template>
     <div class="container">
-        <h1 v-if="this.loginStore.userID !== ''">Vote: {{ userVote }}</h1>
-        <h1 v-else>Login to vote.</h1>
+        <h1 v-if="this.loginStore.userID !== ''">Your vote: {{ userVote }}</h1>
+        <h1 class='loginWarning' v-else>Login to vote.</h1>
         <div class="buttonContainer" v-if="userVote == 'None' && this.loginStore.userID !== ''">
             <div class="votePrompt">How do you feel this article leans?</div>
-            <button class="left" @click="newVote('Left')">Left</button>
-            <button class="right" @click="newVote('Right')">Right</button>
+            <button class="presser" id='left' @click="newVote('Left')">Left</button>
+            <button class="presser" id='right' @click="newVote('Right')">Right</button>
         </div>
         <div class="resultContainer" v-else>
             <h2 class="title">Bias Meter</h2>
             <h3 class="news">News Organization: {{ publisher }}</h3>
             <progress class="bias" id="newsBias" max='100' :value="publisherBias"></progress>
-            <h3 class>Author: {{ author }}</h3>
-            <progress class="bias" max="100" value="50"></progress>
-            <h3>Users</h3>
+            <h3>How our users voted:</h3>
             <progress class="bias" :max="totalVotes" :value="totalVotesRight"></progress>
         </div>
     </div>
 </template>
 
 <style>
+.bias{
+    margin-bottom: 10px;
+}
 
+.title {
+    margin-top: 20px;
+    margin-bottom: 10px;
+}
+.presser {
+    border: none;
+    border-radius: 12px;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+}
+
+.presser:hover {
+    box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
+}
+
+#left {
+    background-color: #008cba;
+}
+
+#right {
+    background-color: #f44336;
+}
+
+.votePrompt {
+    margin-top: 20px;
+    margin-bottom: 10px;
+    text-align: left;
+}
 
 .bias::-webkit-progress-bar {
-    background-color: rgb(145, 145, 255);
+    background-color: #008cba;
 }
 
 .bias::-webkit-progress-value {
-    background-color: rgb(255, 128, 128);
+    background-color: #f44336;
 }
 
 .bias {
-    background-color: rgb(145, 145, 255);
+    background-color: #008cba;
 }
 
 .bias::-moz-progress-bar {
-    background-color: rgb(255, 128, 128);
+    background-color: #f44336;
 }
 </style>
 
