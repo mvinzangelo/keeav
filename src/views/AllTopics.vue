cfc40304d50edc816db655085616f14fd635e615
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2022-08-24 11:44:53 +0200

    Rename files

---
+++ src/views/AllTopics.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+import TopicsList from '../components/TopicsList.vue'
+</script>
+
+<template>
+    <h1>This is the topics</h1>
+    <TopicsList></TopicsList>
+</template>
--- src/views/Topics.vue
+++
@@ -1,7 +0,0 @@
-<script setup>
-
-</script>
-
-<template>
-    <h1>This is the topics</h1>
-</template>
