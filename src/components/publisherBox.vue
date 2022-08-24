28f626683cc587453dc1d363a8c0591595e34bc7
Kirsten821 <kirstenswilliams821@gmail.com>
2022-08-24 13:42:11 +0200

    Styled the MVP of Publisher page

--- src/components/publisherBox.vue
+++ src/components/publisherBox.vue
@@ -25,7 +25,10 @@ export default {
 </div>
 <div>
     <h2 class = "pname"> {{publisherName}}</h2>
-    <progress class="bias" max='100' :value="publisherBias"></progress>
+    <div class="bias">
+        <p>Publisher Bias:</p>
+        <progress class="biasbar" max='100' :value="publisherBias"></progress>
+    </div>
 </div>
 <!-- <h2 class = "pname"> {{publisherName}}</h2> -->
 <!-- <progress class="bias" max='100' :value="publisherBias"></progress> -->
@@ -33,5 +36,33 @@ export default {
 <style scoped>
     .ImageLogo {
         width: 100px;
+        border-radius: 100%;
+        
+    }
+    .pname {
+        text-align: center;
+        padding: 10px;
+        margin-bottom: 25px;
+
+    }
+    .bias {
+        margin-left: 20px;
+        background-color: #cd8de5ff;
+    }
+    /* Below if from Eddie's Voting.vue for reference on bias */
+    .biasbar::-webkit-progress-bar {
+        background-color: #008cba;
+    }
+
+    .biasbar::-webkit-progress-value {
+        background-color: #f44336;
+    }
+
+    .biasbar {
+        background-color: #008cba;
+    }
+
+    .biasbar::-moz-progress-bar {
+        background-color: #f44336;
     }
 </style>
