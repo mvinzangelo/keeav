a3445768ede768097d8a4f06c8adc1c2a1d1f4c3
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2022-08-24 13:07:13 +0200

    Add topic list

--- src/components/TopicsList.vue
+++ src/components/TopicsList.vue
@@ -1,7 +1,53 @@
+<script>
+import { db } from '../firebaseResources';
+import {
+    collection,
+    doc,
+    addDoc,
+    getDoc,
+    getDocs,
+    setDoc,
+    query,
+    where,
+    deleteDoc,
+} from 'firebase/firestore'
+import TopicBox from '../components/TopicBox.vue';
+
+// local functions can be decalred here
+export default {
+    props: {
+    },
+    data() {
+        return {
+            topicsArray: [],
+        };
+    },
+    methods: {
+        async getTopicsDB() {
+            try {
+                let pcluster = query(collection(db, ("topics")));
+                const qSnap = await getDocs(pcluster); //pull database docs from firebase
+                qSnap.forEach((rdoc) => {
+                    this.topicsArray.push({ topicID: rdoc.id, topicData: rdoc.data() });
+                });
+            }
+            catch (e) {
+                alert("retrieve" + e);
+            }
+        },
+    },
+    beforeMount() {
+        this.getTopicsDB();
+    },
+    components: { TopicBox }
+}
+</script>
+
 <template>
-    <button></button>
+    <div v-for="(topic, index) in topicsArray" class="topics-container">
+        <TopicBox :topicID="topicsArray[index].topicID" :topic="topicsArray[index].topicData.topic"
+            :date="topicsArray[index].topicData.date">
+        </TopicBox>
+    </div>
 </template>
 
-<script setup>
-
-</script>
